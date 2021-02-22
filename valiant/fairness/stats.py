7415a7d8d6111cfc37392d83ed00857fe1d7ae40
DataBay <yongyibay@gmail.com>
2021-02-22 15:19:19 -0600

    new docs

--- valiant/fairness/stats.py
+++ valiant/fairness/stats.py
@@ -52,19 +52,16 @@ class Tost:
         :param print_output: Prints the dataframe results of the ANOVA analysis. Default is True
         :type print_output: Boolean, optional
         :param usevar: 'pooled' or 'unequal'
-
             - If 'pooled', then the standard deviation of the samples is assumed to be the same\n
             - If 'unequal', then Welsh ttest with Satterthwait degrees of freedom is used
         :type usevar: string, optional
         :param weights: Case weights for the two samples. Default is None
         :type weights: tuples of None or ndarrays, optional
         :param transform: transformation function
-
             - If None (default), then the data is not transformed\n
             - Given a function, sample data and thresholds are transformed. If transform is log, then the equivalence interval is in ratio: low < m1 / m2 < upp
         :type transform: None or function, optional
         :return: Dataframe with the following fields:
-
                 - pvalue (float): pvalue of the non-equivalence test\n
                 - t1, pv1 (tuple of floats): test statistic and pvalue for lower threshold test\n
                 - t2, pv2 (tuple of floats): test statistic and pvalue for upper threshold test
